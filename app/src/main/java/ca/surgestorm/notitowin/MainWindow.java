bfea6e2d9007a925034967735238cd839cb93885
maximum-maxness <maxwritchie@gmail.com>
2018-09-24 14:37:13 -0600

    Test

--- app/src/main/java/ca/surgestorm/notitowin/MainWindow.java
+++ app/src/main/java/ca/surgestorm/notitowin/MainWindow.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 
 public class MainWindow extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
-    }
-}
+        }
+        }
